8dd03de630cc7957922a3a5f0e65bb34c4a1a089
juru-juliette <kangabejuliette@gmail.com>
2019-09-26 10:39:52 +0200

    creating models

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ from datetime import datetime
 class User(UserMixin,db.Model):
     __tablename__='users'
 
-    id = db.Column(db.Integer,primary_key=True)
+    id = db.column(db.Integer,primary_key=True)
     username= db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True , index=True)
     bio= db.Column(db.String(255))
